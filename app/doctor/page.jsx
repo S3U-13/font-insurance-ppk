efd5735e8ef9ea1dccb08a9f75e9b4d2f9538d64
unknown <ppk@ICT-DEV-NES.Leelawadee.go.th>
2025-11-25 15:51:52 +0700

    version 1.0.1

--- app/doctor/page.jsx
+++ app/doctor/page.jsx
@@ -8,24 +8,36 @@ import ModalIPD from "./create_form_ipd/page";
 import ModalOPD from "./create_form_opd/page";
 
 export default function page() {
-  const { setOpenDrawer } = useDrawer();
+  const { openDrawer, setOpenDrawer } = useDrawer();
   const { openModalIPD, setOpenModalIPD, openModalOPD, setOpenModalOPD } =
     useHook();
   return (
     <div>
       <Button onPress={() => setOpenDrawer(true)} isIconOnly>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 20 20"
-          fill="currentColor"
-          className="size-5"
-        >
-          <path
-            fillRule="evenodd"
-            d="M2 4.75A.75.75 0 0 1 2.75 4h14.5a.75.75 0 0 1 0 1.5H2.75A.75.75 0 0 1 2 4.75ZM2 10a.75.75 0 0 1 .75-.75h14.5a.75.75 0 0 1 0 1.5H2.75A.75.75 0 0 1 2 10Zm0 5.25a.75.75 0 0 1 .75-.75h14.5a.75.75 0 0 1 0 1.5H2.75a.75.75 0 0 1-.75-.75Z"
-            clipRule="evenodd"
-          />
-        </svg>
+        {openDrawer === false && (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+            className="size-5"
+          >
+            <path
+              fillRule="evenodd"
+              d="M2 4.75A.75.75 0 0 1 2.75 4h14.5a.75.75 0 0 1 0 1.5H2.75A.75.75 0 0 1 2 4.75ZM2 10a.75.75 0 0 1 .75-.75h14.5a.75.75 0 0 1 0 1.5H2.75A.75.75 0 0 1 2 10Zm0 5.25a.75.75 0 0 1 .75-.75h14.5a.75.75 0 0 1 0 1.5H2.75a.75.75 0 0 1-.75-.75Z"
+              clipRule="evenodd"
+            />
+          </svg>
+        )}
+        {openDrawer === true && (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+            className="size-5 "
+          >
+            <path d="M6.28 5.22a.75.75 0 0 0-1.06 1.06L8.94 10l-3.72 3.72a.75.75 0 1 0 1.06 1.06L10 11.06l3.72 3.72a.75.75 0 1 0 1.06-1.06L11.06 10l3.72-3.72a.75.75 0 0 0-1.06-1.06L10 8.94 6.28 5.22Z" />
+          </svg>
+        )}
       </Button>
       <Button onPress={() => setOpenModalIPD(true)}>OpenIPD</Button>
       <Button onPress={() => setOpenModalOPD(true)}>OpenOPD</Button>
--- components/sidebar.jsx
+++ components/sidebar.jsx
@@ -7,6 +7,7 @@ import {
   DrawerFooter,
   DrawerHeader,
 } from "@heroui/drawer";
+import { Tab, Tabs } from "@heroui/tabs";
 import React from "react";
 
 export default function Sidebar({ isOpen, onClose }) {
@@ -18,27 +19,74 @@ export default function Sidebar({ isOpen, onClose }) {
           {(onClose) => (
             <>
               <DrawerHeader className="flex flex-col gap-1">
-                Side Bar
+                <h1 className="text-center">Side Bar</h1>
               </DrawerHeader>
               <DrawerBody>
-                <p>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                  Nullam pulvinar risus non risus hendrerit venenatis.
-                  Pellentesque sit amet hendrerit risus, sed porttitor quam.
-                </p>
-                <p>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                  Nullam pulvinar risus non risus hendrerit venenatis.
-                  Pellentesque sit amet hendrerit risus, sed porttitor quam.
-                </p>
-                <p>
-                  Magna exercitation reprehenderit magna aute tempor cupidatat
-                  consequat elit dolor adipisicing. Mollit dolor eiusmod sunt ex
-                  incididunt cillum quis. Velit duis sit officia eiusmod Lorem
-                  aliqua enim laboris do dolor eiusmod. Et mollit incididunt
-                  nisi consectetur esse laborum eiusmod pariatur proident Lorem
-                  eiusmod et. Culpa deserunt nostrud ad veniam.
-                </p>
+                <Tabs
+                  aria-label="Options"
+                  placement="start"
+                  classNames={{
+                    tabList: "w-full p-2",
+                    base: "w-full",
+                  }}
+                >
+                  <Tab
+                    key="photos"
+                    title={
+                      <div className="flex items-center space-x-2">
+                        <span>Photos</span>
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          viewBox="0 0 20 20"
+                          fill="currentColor"
+                          className="size-5"
+                        >
+                          <path
+                            fillRule="evenodd"
+                            d="M1 5.25A2.25 2.25 0 0 1 3.25 3h13.5A2.25 2.25 0 0 1 19 5.25v9.5A2.25 2.25 0 0 1 16.75 17H3.25A2.25 2.25 0 0 1 1 14.75v-9.5Zm1.5 5.81v3.69c0 .414.336.75.75.75h13.5a.75.75 0 0 0 .75-.75v-2.69l-2.22-2.219a.75.75 0 0 0-1.06 0l-1.91 1.909.47.47a.75.75 0 1 1-1.06 1.06L6.53 8.091a.75.75 0 0 0-1.06 0l-2.97 2.97ZM12 7a1 1 0 1 1-2 0 1 1 0 0 1 2 0Z"
+                            clipRule="evenodd"
+                          />
+                        </svg>
+                      </div>
+                    }
+                  ></Tab>
+                  <Tab
+                    key="music"
+                    title={
+                      <div className="flex items-center space-x-2">
+                        <span>Music</span>
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          viewBox="0 0 20 20"
+                          fill="currentColor"
+                          className="size-5"
+                        >
+                          <path
+                            fillRule="evenodd"
+                            d="M17.721 1.599a.75.75 0 0 1 .279.583v11.29a2.25 2.25 0 0 1-1.774 2.2l-2.041.44a2.216 2.216 0 0 1-.938-4.332l2.662-.577a.75.75 0 0 0 .591-.733V6.112l-8 1.73v7.684a2.25 2.25 0 0 1-1.774 2.2l-2.042.44a2.216 2.216 0 1 1-.935-4.331l2.659-.573A.75.75 0 0 0 7 12.529V4.236a.75.75 0 0 1 .591-.733l9.5-2.054a.75.75 0 0 1 .63.15Z"
+                            clipRule="evenodd"
+                          />
+                        </svg>
+                      </div>
+                    }
+                  ></Tab>
+                  <Tab
+                    key="videos"
+                    title={
+                      <div className="flex items-center space-x-2">
+                        <span>Videos</span>
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          viewBox="0 0 20 20"
+                          fill="currentColor"
+                          className="size-5"
+                        >
+                          <path d="M3.25 4A2.25 2.25 0 0 0 1 6.25v7.5A2.25 2.25 0 0 0 3.25 16h7.5A2.25 2.25 0 0 0 13 13.75v-7.5A2.25 2.25 0 0 0 10.75 4h-7.5ZM19 4.75a.75.75 0 0 0-1.28-.53l-3 3a.75.75 0 0 0-.22.53v4.5c0 .199.079.39.22.53l3 3a.75.75 0 0 0 1.28-.53V4.75Z" />
+                        </svg>
+                      </div>
+                    }
+                  ></Tab>
+                </Tabs>
               </DrawerBody>
               <DrawerFooter>
                 <Button color="danger" variant="light" onPress={onClose}>
